7c62c90f5de92cd2517b6e9b8030816eb2b62123
Divya Garg <divyagarg2601@gmail.com>
2021-01-20 16:18:29 +0530

    showing todos in todos/id page

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,11 @@
 class TodosController < ApplicationController
-    def index
-        render plain: Todo.all.to_a
-    end
+  def index
+    render plain: Todo.all.map { |todo| todo.to_displayable_string }
+  end
+
+  def show
+    id = params[:id]
+    todos = Todo.where(id: id)
+    render plain: todos.map { |todo| todo.to_displayable_string }
+  end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,6 @@
 class Todo < ActiveRecord::Base
-
+  def to_displayable_string
+    isCompleted = completed ? "[X]" : "[ ]"
+    "#{isCompleted} #{due_date.to_s(:long)} #{todo_text} #{completed}"
+  end
 end
