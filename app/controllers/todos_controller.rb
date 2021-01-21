11a98ff245ef54670b8ec0b2a8a997253a53890e
Divya Garg <divyagarg2601@gmail.com>
2021-01-21 14:15:19 +0530

    created post request using postman

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render plain: Todo.all.map { |todo| todo.to_displayable_string }
   end
@@ -7,5 +9,16 @@ class TodosController < ApplicationController
     id = params[:id]
     todos = Todo.where(id: id)
     render plain: todos.map { |todo| todo.to_displayable_string }
+  end
+
+  def create
+    todo_text = params["todo_text"]
+    due_date = DateTime.parse(params["due_date"])
+    Todo.create!(
+        todo_text: todo_text,
+        due_date: due_date,
+        completed: false
+    )
+    render plain: "done"
   end
 end
