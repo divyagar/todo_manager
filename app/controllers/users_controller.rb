249a08c67ba1ba9489d402116408450f6fa37e5f
Divya Garg <divyagarg2601@gmail.com>
2021-01-21 17:12:24 +0530

    added functionality to create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
-    render plain: User.all
+    render plain: User.all.map { |user| user.to_displayable_string}.join("\n")
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+
+    User.create!(
+      name: name,
+      email: email,
+      password: password
+    )
+    render plain: "done"
   end
 end
