beafffa91eb31b12199ecebfc504efb03b8557d3
Divya Garg <divyagarg2601@gmail.com>
2021-01-24 11:30:23 +0530

    completed assignment 7. Deploying website is remaining

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,22 @@ class Todo < ActiveRecord::Base
     isCompleted = completed ? "[X]" : "[ ]"
     "#{id} #{due_date.to_s(:long)} #{todo_text} #{isCompleted}"
   end
+  
+  def self.overdue
+    all.where("due_date < ? and completed = ?", Date.today, false)
+  end
+
+  def self.due_today
+    all.where(due_date: Date.today)
+  end
+  
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.isCompleted
+    all.where(completed: true)
+  end
+
+
 end
